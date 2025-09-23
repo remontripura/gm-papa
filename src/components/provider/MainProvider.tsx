611aced284c2804fe3eb0e228a870dad5084943d
remontripura <remontripura045@gmail.com>
2025-09-23 07:05:32 +0600

    udpate

--- src/components/provider/MainProvider.tsx
+++ src/components/provider/MainProvider.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import type React from "react";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
-import 'react-loading-skeleton/dist/skeleton.css'
-
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect } from "react";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => console.log("Service Worker registered:", reg))
+        .catch((err) => console.log("SW registration failed:", err));
+    }
+  }, []);
+
   return (
     <SessionProvider>
       {" "}
