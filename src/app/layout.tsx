39d98f8d8127ce6b210751e1efe8732e037249fe
remontripura <remontripura045@gmail.com>
2025-10-06 12:40:41 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "##1E2131" },
-    { media: "(prefers-color-scheme: dark)", color: "##1E2131" },
+    { media: "(prefers-color-scheme: light)", color: "#1E2131" },
+    { media: "(prefers-color-scheme: dark)", color: "#1E2131" },
   ],
 };
 
