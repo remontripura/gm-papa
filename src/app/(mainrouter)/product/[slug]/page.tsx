eb318cf2bfbfd043b20bae1510351a7f0249b34f
remontripura <remontripura045@gmail.com>
2025-10-08 07:16:17 +0600

    udpate conical

--- src/app/(mainrouter)/product/[slug]/page.tsx
+++ src/app/(mainrouter)/product/[slug]/page.tsx
@@ -25,6 +25,9 @@ export async function generateMetadata({
   return {
     title: singleProduct?.seo_title || singleProduct.name || "Product",
     description: singleProduct?.seo_description || "Check out this product",
+    alternates: {
+      canonical: `${process.env.NEXTAUTH_URL}/product/${params.slug}`,
+    },
     keywords: singleProduct?.seo_keywords,
     openGraph: {
       title: singleProduct?.seo_title || singleProduct.name || "Product",
