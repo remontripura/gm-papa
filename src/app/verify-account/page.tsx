316c2dd3c35df45c7cdeaf1c76246f62312b5c1d
remontripura <remontripura045@gmail.com>
2025-10-09 09:09:50 +0600

    update

--- src/app/verify-account/page.tsx
+++ src/app/verify-account/page.tsx
@@ -3,9 +3,6 @@ import VarifyAccountCompo from "./VerifyAccount";
 
 export const metadata: Metadata = {
   title: `verify account | ${process.env.NEXT_PUBLIC_APP_NAME}`,
-  description: "verify account",
-  keywords:
-    "Free Fire top up Bangladesh, freefirebd, cheap diamond top up, gaming offers",
 };
 
 const VarifyAccount = () => {
